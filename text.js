9f39817c43a132dcb913b0c0c9ceac904ca3966c
jasjeetjhally <jjsingh@rocketmail.com>
2016-01-12 23:19:35 +0000

    Test commit

---
+++ text.js
@@ -0,0 +1,6 @@
+var image1 = document.getElementById("image1");
+var dec1 = document.getElementById("dec1");
+
+image1.addEventListener("click", function(){
+    dec1.innerHTML = "DJ Khaled responses with, “How you doin?”";
+});
